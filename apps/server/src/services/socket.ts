f27c97c6e02db1c7ebbd57a487d030f5c75eff99
Surkhab Singh <surkhab26singh@gmail.com>
2025-02-06 15:06:28 +0530

    added redis for broadcasting

---
+++ apps/server/src/services/socket.ts
@@ -0,0 +1,44 @@
+import { Server } from "socket.io";
+import Redis from "ioredis";
+
+const pub = new Redis({
+  host: "127.0.0.1",
+  port: 6379, 
+});
+const sub = new Redis({
+  host: "127.0.0.1",
+  port: 6379,
+});
+
+class SocketService {
+  private _io: Server;
+  constructor() {
+    console.log("init socket service....");
+    this._io = new Server({
+      cors: {
+        allowedHeaders: ["*"],
+        origin: "*",
+      },
+    });
+  }
+
+  public initListeners() {
+    const io = this.io;
+    console.log("init socket listener");
+    io.on("connect", (socket) => {
+      console.log("new socket connected", socket.id);
+
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("new message received", message);
+
+        await pub.publish("MESSAGES", JSON.stringify({ message }));
+      });
+    });
+  }
+
+  get io() {
+    return this._io;
+  }
+}
+
+export default SocketService;
